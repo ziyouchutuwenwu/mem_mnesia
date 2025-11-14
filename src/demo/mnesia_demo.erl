eeeee3d6d5f60cd75968f21452fcd15fa444d1bb
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2025-11-14 13:33:48 +0800

    .

--- src/demo/mnesia_demo.erl
+++ src/demo/mnesia_demo.erl
@@ -1,39 +1,66 @@
 -module(mnesia_demo).
 
--record(
-	person, {
-		name,
-		sex,
-		age
+-record(person, {
+    name,
+    sex,
+    age
 }).
 
--export([init/0, create_table/0]).
--export([write/0, delete_table/1]).
+-export([init/0]).
+-export([create_table/0, delete_table/1]).
+-export([read/1, batch_read/0]).
+-export([write/0, batch_write/0]).
 
-init()->
-	application:start(mem_mnesia).
+init() ->
+    application:start(mem_mnesia).
 
-create_table()->
-	mnesia:create_table(person, [
-		{ram_copies, [node()]},
-		{attributes, record_info(fields, person)}
-	]).
+create_table() ->
+    mnesia:create_table(person, [
+        {ram_copies, [node()]},
+        {attributes, record_info(fields, person)}
+    ]).
 
 delete_table(Id) ->
-	Fun = fun() ->
-			mnesia:delete({person, Id})
-	      end,
-	mnesia:transaction(Fun).
+    Fun = fun() ->
+        mnesia:delete({person, Id})
+    end,
+    mnesia:transaction(Fun).
 
-write()->
-	LoopCount = lists:seq(1, 1000),
-	lists:foreach(
-		fun(Index) ->
-			F = fun() ->
-					Rec = #person{name=Index, sex = man, age = Index},
-					mnesia:write(Rec)
-			    end,
-			mnesia:transaction(F)
-		end,
-		LoopCount
-	).
+read(Age) ->
+    Fun = fun() ->
+        Match = #person{age = Age, _ = '_'},
+        mnesia:match_object(Match)
+    end,
+    mnesia:transaction(Fun).
+
+write() ->
+    F = fun() ->
+        Rec = #person{name = ~"aaa", sex = man, age = 20},
+        mnesia:write(Rec)
+    end,
+    mnesia:transaction(F).
+
+batch_read() ->
+    Fun = fun() ->
+        mnesia:foldl(
+            fun(Record, Acc) ->
+                [Record | Acc]
+            end,
+            [],
+            person
+        )
+    end,
+    mnesia:transaction(Fun).
+
+batch_write() ->
+    LoopCount = lists:seq(1, 1000),
+    lists:foreach(
+        fun(Index) ->
+            F = fun() ->
+                Rec = #person{name = Index, sex = man, age = Index},
+                mnesia:write(Rec)
+            end,
+            mnesia:transaction(F)
+        end,
+        LoopCount
+    ).
